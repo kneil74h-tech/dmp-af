edac742014d21809312bf520aa6664f60bbfc6b7
Macbook <k.neil74h@gmail.com>
2025-10-31 22:20:42 +0300

    Revert "dict -> set"
    This reverts commit 6b5f6a03d9d4289281118e76f6cf02917818027b.

--- dmp_af/dags.py
+++ dmp_af/dags.py
@@ -27,7 +27,7 @@ def dbt_main_dags(graph: DmpAfGraph) -> dict[str, DAG]:
 
     dag_callbacks, task_callbacks = collect_af_custom_callbacks(graph.config)
     domains = {
-        node.domain_dag
+        node.domain_dag: node
         for node in graph.nodes
         if not graph.etl_service_name
         or not node.etl_service_name
