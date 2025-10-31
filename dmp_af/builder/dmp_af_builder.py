25f5398619e0a7b6856610da1cae7f55b1467869
Niall <k.neil74h@gmail.com>
2025-10-31 16:38:16 +0300

    Revert "fix skipping nodes"
    This reverts commit 24d776651839f30d0be11ae5a61714dee86fbe4c.

--- dmp_af/builder/dmp_af_builder.py
+++ dmp_af/builder/dmp_af_builder.py
@@ -163,15 +163,11 @@ class DmpAfGraph:
                 continue
 
         self._models = {
-            node.unique_id: self._dag_components_registry[node.unique_id]
-            for node in nodes
-            if not node.is_test() and node.unique_id in self._dag_components_registry
+            node.unique_id: self._dag_components_registry[node.unique_id] for node in nodes if not node.is_test()
         }
         if not backfill:
             self._large_tests = {
-                node.unique_id: self._dag_components_registry[node.unique_id]
-                for node in nodes
-                if node.is_large_test() and node.unique_id in self._dag_components_registry
+                node.unique_id: self._dag_components_registry[node.unique_id] for node in nodes if node.is_large_test()
             }
 
     def _collect_maintenance_components(self) -> None:
